55347115f9e3a296e172f250df4a65f2226c427e
Julius M <67582275+sydhfjggt@users.noreply.github.com>
2026-02-26 16:49:33 +0100

    2026-02-26

--- app.rb
+++ app.rb
@@ -20,4 +20,33 @@ class App < Sinatra::Base
         erb :index
     end
 
+    get '/results' do
+        @results = db.execute('SELECT * FROM results')
+        p @todos
+        erb(:"todos/index")
+    end
+
+    get '/results/new' do
+        erb(:"results/new")
+    end
+
+    post '/results' do
+        p params
+
+        db.execute("INSERT INTO results () VALUES(?,?)", [params["result_"], params["result_"]])
+        redirect("/results")
+    end
+
+    get'/results/:id' do | id |
+        @result = db.execute('SELECT * FROM results WHERE id=?', id).first
+        erb(:"results/show")
+    end
+
+    post '/results/:id/delete' do | id |
+        db.execute("DELETE FROM results WHERE id=?", id)
+        redirect("/results")
+    end
+
+    get
+
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,7 +3,45 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    puts "Using db file: #{DB_PATH}"
+    puts "🧹 Dropping old tables..."
+    drop_tables
+    puts "🧱 Creating tables..."
+    create_tables
+    puts "🍎 Populating tables..."
+    populate_tables
+    puts "✅ Done seeding the database!"
+  end
+
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS results')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE results (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                category_id INTEGER, 
+                description TEXT)')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO results () VALUES ("")')
+    db.execute('INSERT INTO results () VALUES ("")')
+    db.execute('INSERT INTO results () VALUES ("")')
+  end
+
+  private
+
+  def self.db
+    @db ||= begin
+      db = SQLite3::Database.new(DB_PATH)
+      db.results_as_hash = true
+      db
+    end
   end
 
 end
+
+Seeder.seed!
+  
